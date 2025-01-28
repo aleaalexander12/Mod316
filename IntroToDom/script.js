f0b907d5cda4f9d446a3a1c500f640e5d5030310
Lea Simone <apple@MacBookPro.attlocal.net>
2025-01-28 17:15:26 -0600

    initial commit

---
+++ IntroToDom/script.js
@@ -0,0 +1,46 @@
+console.log(document.body);
+
+
+
+const pTag = document.getElementById("intro")
+
+console.log(pTag)
+
+
+console.dir(document.body.children[1].innerText);
+
+
+console.log(document.scripts)
+
+
+const ul = document.querySelector('ul')
+
+console.log(ul.firstElementChild)
+console.log(ul.lastElementChild)
+
+console.log(ul.childNodes[3])
+
+const els = ul.firstElementChild.nextElementSibling.childNodes
+console.log(els)
+
+const newAnchor = document.createElement("a")
+newAnchor.textContent = "Second Anchor"
+
+console.log(newAnchor)
+
+ul.firstElementChild.nextElementSibling.appendChild(newAnchor)
+
+
+// query the div with id app
+const app = document.querySelector("#app")
+console.log(app)
+
+// append a new child div
+app.appendChild(document.createElement("div"))
+
+// create a new p element and  set the text content
+const p = document.createElement("p")
+p.textContent = "Hello"
+
+// append the p element to the div with id app 
+app.appendChild(p)
